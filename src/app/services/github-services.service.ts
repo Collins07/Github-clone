a7055be8242a2b9fbe096424317690d24a275034
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-04-17 10:07:31 +0300

    functions to display

--- src/app/services/github-services.service.ts
+++ src/app/services/github-services.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 import { CLIENT_ID, CLIENT_SECRET } from '../GithubCred';
 
 @Injectable({
@@ -10,12 +12,32 @@ export class GithubServicesService {
   constructor(private httpClient: HttpClient) { }
 
   //for github profiles
-  public getProfile(searchQuery: any){
-    let dataURL = `https://api.github.com/users/${searchQuery}? client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+  public getProfile(searchQuery: any):Observable<any>{
+    let dataURL = `https://api.github.com/users/${searchQuery}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`;
+    return this.httpClient.get<any>(dataURL).pipe(retry(),);
   }
 
 
   //for github repos
-  //
+  public getRepos(searchQuery: any):Observable<any[]>{
+    let dataURL = `https://api.github.com/users/${searchQuery}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`;
+    return this.httpClient.get<any[]>(dataURL).pipe(retry(),);
+  }
+
+  // public handleErrors(errors: HttpErrorResponse){
+  //   let errorMessage:string;
+  //   if(error.error instanceof ErrorEvent){
+  //     //clientside error
+  //     errorMessage= `MESSAGE: ${error.error.message}`;
+  //   }
+  //   else {
+  //     errorMessage= `STATUS: ${error.status} MESSAGE: ${error.message}`;
+  //   }
+  //   return throwError(errorMessage);
+  // }
 
 }
+function count1(count1: any): import("rxjs").OperatorFunction<Object, Object> {
+  throw new Error('Function not implemented.');
+}
+
