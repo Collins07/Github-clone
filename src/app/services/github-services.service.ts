93a41b2756f47dce6e0c2d7f7a9f2834de4e4896
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-04-19 06:53:40 +0300

    worked on some errors

--- src/app/services/github-services.service.ts
+++ src/app/services/github-services.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
-import { catchError, count, retry } from 'rxjs/operators';
+import { catchError, count, retry,map } from 'rxjs/operators';
 import { CLIENT_ID, CLIENT_SECRET } from '../GithubCred';
 
 @Injectable({
@@ -24,17 +24,7 @@ export class GithubServicesService {
     return this.httpClient.get<any[]>(dataURL).pipe(retry(),);
   }
 
-  // public handleErrors(errors: HttpErrorResponse){
-  //   let errorMessage:string;
-  //   if(error.error instanceof ErrorEvent){
-  //     //clientside error
-  //     errorMessage= `MESSAGE: ${error.error.message}`;
-  //   }
-  //   else {
-  //     errorMessage= `STATUS: ${error.status} MESSAGE: ${error.message}`;
-  //   }
-  //   return throwError(errorMessage);
-  // }
+ 
 
 }
 function count1(count1: any): import("rxjs").OperatorFunction<Object, Object> {
